bd97dfbd18fa39d4d259ec39f748901f23984267
LEchn <le@users.noreply.github.com>
2020-03-16 17:23:02 +0800

    lelab3 for ubuntu

---
+++ leab3/process.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/times.h>
+
+#define HZ	100
+
+void cpuio_bound(int last, int cpu_time, int io_time);
+
+int main(int argc, char * argv[])
+{
+	int n_process[10];
+	int i;
+	pid_t pid;
+	for(i=0;i<10;i++)
+	  {
+		   pid=fork();
+		   if（pid<0）
+		     {
+				 printf("error");
+			 }
+			 else
+			 {
+				 if(pid == 0 )
+				   {
+					   n_processp[i]=pid;
+					   cpuio_bund(20,2*i,20-2*i);
+
+					   return 0;
+				   }
+			 }
+			 
+	  }
+	 
+	for(i=0;i<10;i++)
+	 printf("child pid : %d",n_process[i]);
+
+	wait(&i);
+			/* 父进程一旦调用了wait就立即阻塞自己，由wait自动分析是否当前进程的某个子进程已经退出，如果让它找到了这样一个已经变成僵尸的子进程，wait就会收集这个子进程的信息，并把它彻底销毁后返回；
+			如果没有找到这样一个子进程，wait就会一直阻塞在这里，直到有一个出现为止。
+			当父进程忘了用wait()函数等待已终止的子进程时,子进程就会进入一种无父进程的状态,此时子进程就是僵尸进程.
+			wait()要与fork()配套出现,如果在使用fork()之前调用wait(),wait()的返回值则为-1,正常情况下wait()的返回值为子进程的PID.
+			如果先终止父进程,子进程将继续正常进行，只是它将由init进程(PID 1)继承,当子进程终止时,init进程捕获这个状态.参数status用来保存被收集进程退出时的一些状态，它是一个指向int类型的指针。
+			但如果我们对这个子进程是如何死掉毫不在意，只想把这个僵尸进程消灭掉，（事实上绝大多数情况下，我们都会这样想），我们就可以设定这个参数为NULL*/
+
+	return 0;
+}
+
+/*
+ * 此函数按照参数占用CPU和I/O时间
+ * last: 函数实际占用CPU和I/O的总时间，不含在就绪队列中的时间，>=0是必须的
+ * cpu_time: 一次连续占用CPU的时间，>=0是必须的
+ * io_time: 一次I/O消耗的时间，>=0是必须的
+ * 如果last > cpu_time + io_time，则往复多次占用CPU和I/O
+ * 所有时间的单位为秒
+ */
+void cpuio_bound(int last, int cpu_time, int io_time)
+{
+	struct tms start_time, current_time;
+	clock_t utime, stime;
+	int sleep_time;
+
+	while (last > 0)
+	{
+		/* CPU Burst */
+		times(&start_time);
+		/* 其实只有t.tms_utime才是真正的CPU时间。但我们是在模拟一个
+		 * 只在用户状态运行的CPU大户，就像“for(;;);”。所以把t.tms_stime
+		 * 加上很合理。*/
+		do
+		{
+			times(&current_time);
+			utime = current_time.tms_utime - start_time.tms_utime;
+			stime = current_time.tms_stime - start_time.tms_stime;
+		} while ( ( (utime + stime) / HZ )  < cpu_time );
+		last -= cpu_time;
+
+		if (last <= 0 )
+			break;
+
+		/* IO Burst */
+		/* 用sleep(1)模拟1秒钟的I/O操作 */
+		sleep_time=0;
+		while (sleep_time < io_time)
+		{
+			sleep(1);
+			sleep_time++;
+		}
+		last -= sleep_time;
+	}
+}
