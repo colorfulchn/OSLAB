29c84b64425bc0e430734a0d77e2f05e26fa8aeb
lechn <35887707+colorfulchn@users.noreply.github.com>
2020-03-17 13:39:51 +0800

    Update process.c

--- leab3/process.c
+++ leab3/process.c
@@ -1,7 +1,17 @@
+/* 父进程一旦调用了wait就立即阻塞自己，由wait自动分析是否当前进程的某个子进程已经退出，如果让它找到了这样一个已经变成僵尸的子进程，wait就会收集这个子进程的信息，并把它彻底销毁后返回；
+			如果没有找到这样一个子进程，wait就会一直阻塞在这里，直到有一个出现为止。
+			当父进程忘了用wait()函数等待已终止的子进程时,子进程就会进入一种无父进程的状态,此时子进程就是僵尸进程.
+			wait()要与fork()配套出现,如果在使用fork()之前调用wait(),wait()的返回值则为-1,正常情况下wait()的返回值为子进程的PID.
+			如果先终止父进程,子进程将继续正常进行，只是它将由init进程(PID 1)继承,当子进程终止时,init进程捕获这个状态.参数status用来保存被收集进程退出时的一些状态，它是一个指向int类型的指针。
+			但如果我们对这个子进程是如何死掉毫不在意，只想把这个僵尸进程消灭掉，（事实上绝大多数情况下，我们都会这样想），我们就可以设定这个参数为NULL*/
+
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/times.h>
+#include <sys/types.h>
 
 #define HZ	100
 
@@ -11,39 +21,25 @@ int main(int argc, char * argv[])
 {
 	pid_t n_process[10];
 	int i;
-	pid_t pid;
 	for(i=0;i<10;i++)
-	  {
-		   pid=fork();
-		   if（pid<0）
-		     {
-				 printf("error");
-				 return -1;
-			 }
-			 else
-			 {
-				 if(pid == 0 )
-				   {
-					   n_processp[i]=pid;
-					   cpuio_bund(20,2*i,20-2*i);
-
-					   return 0;
-				   }
-			 }
-			 
-	  }
+	{
+		n_process[i] = fork();
+		if(n_process[i] < 0)
+		{
+			printf("error");
+			return -1;
+		}
+		else if(n_process[i] == 0 )
+		{	
+			cpuio_bound(20,2*i,20-2*i); 
+			return 0; /*执行完cpuio_bound 以后，结束该子进程*/
+		}
+	}
 	 
 	for(i=0;i<10;i++)
-	 printf("child pid : %d",n_process[i]);
+	 printf("child pid : %d  parent pid: %d\n",n_process[i],getppid());
 
 	wait(&i);
-			/* 父进程一旦调用了wait就立即阻塞自己，由wait自动分析是否当前进程的某个子进程已经退出，如果让它找到了这样一个已经变成僵尸的子进程，wait就会收集这个子进程的信息，并把它彻底销毁后返回；
-			如果没有找到这样一个子进程，wait就会一直阻塞在这里，直到有一个出现为止。
-			当父进程忘了用wait()函数等待已终止的子进程时,子进程就会进入一种无父进程的状态,此时子进程就是僵尸进程.
-			wait()要与fork()配套出现,如果在使用fork()之前调用wait(),wait()的返回值则为-1,正常情况下wait()的返回值为子进程的PID.
-			如果先终止父进程,子进程将继续正常进行，只是它将由init进程(PID 1)继承,当子进程终止时,init进程捕获这个状态.参数status用来保存被收集进程退出时的一些状态，它是一个指向int类型的指针。
-			但如果我们对这个子进程是如何死掉毫不在意，只想把这个僵尸进程消灭掉，（事实上绝大多数情况下，我们都会这样想），我们就可以设定这个参数为NULL*/
-
 	return 0;
 }
 
