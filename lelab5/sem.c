0e17134cdc9997006ed5b9fdda09607eb31810c6
LEchn <le@users.noreply.github.com>
2020-03-22 20:42:17 +0800

    修改2

--- lelab5/sem.c
+++ lelab5/sem.c
@@ -7,23 +7,23 @@
 #include <linux/fdreg.h>
 #include <asm/system.h>
 #include <asm/io.h>
-//#include <string.h>
 
 sem_t semtable[SEMTABLE_LEN];
 int cnt = 0;
 
 sem_t *sys_sem_open(const char *name,unsigned int value)
 {
-    char kernelname[100];	/* 应该足够大了 */
+    char kernelname[100];   /* 应该足够大了 */
     int isExist = 0;
     int i=0;
     int name_cnt=0;
     while( get_fs_byte(name+name_cnt) != '\0')
-	name_cnt++;
+    name_cnt++;
     if(name_cnt>SEM_NAME_LEN)
-	return NULL;
+    return NULL;
     for(i=0;i<name_cnt;i++)
-	kernelname[i]=get_fs_byte(name+i);
+     /* 从用户态复制到内核态 */
+    kernelname[i]=get_fs_byte(name+i);
     int name_len = strlen(kernelname);
     int sem_name_len =0;
     sem_t *p=NULL;
@@ -32,17 +32,17 @@ sem_t *sys_sem_open(const char *name,unsigned int value)
         sem_name_len = strlen(semtable[i].name);
         if(sem_name_len == name_len)
         {
-	    		if( !strcmp(kernelname,semtable[i].name) )
-	    		{
-					isExist = 1;
-					break;
-	    		}
+                if( !strcmp(kernelname,semtable[i].name) )
+                {
+                    isExist = 1;
+                    break;
+                }
         }
     }
     if(isExist == 1)
     {
         p=(sem_t*)(&semtable[i]);
-        //printk("find previous name!\n");
+        /*printk("find previous name!\n");*/
     }
     else
     {
@@ -53,7 +53,7 @@ sem_t *sys_sem_open(const char *name,unsigned int value)
         }
         semtable[cnt].value = value;
         p=(sem_t*)(&semtable[cnt]);
-	     //printk("creat name!\n");
+         /*printk("creat name!\n");*/
         cnt++;
      }
     return p;
@@ -62,35 +62,35 @@ sem_t *sys_sem_open(const char *name,unsigned int value)
 
 int sys_sem_wait(sem_t *sem)
 {
-	cli();
-	while( sem->value <= 0 )		//
-		sleep_on(&(sem->queue));	//这两条语句顺序不能颠倒，很重要，是关于互斥信号量能不
-	sem->value--;				//能正确工作的！！！
-	sti();
-	return 0;
+    cli();
+    while( sem->value <= 0 )        /*使得所有小于0的进程阻塞*/
+        sleep_on(&(sem->queue));    
+    sem->value--;             
+    sti();
+    return 0;
 }
 int sys_sem_post(sem_t *sem)
 {
-	cli();
-	sem->value++;
-	if( (sem->value) <= 1)
-		wake_up(&(sem->queue));
-	sti();
-	return 0;
+    cli();
+    sem->value++;
+    if( (sem->value) <= 1)    /*小于0阻塞，只有为1时才唤醒，保证一次仅唤醒一个进程*/
+        wake_up(&(sem->queue));
+    sti();
+    return 0;
 }
 
 int sys_sem_unlink(const char *name)
 {
-    char kernelname[100];	/* 应该足够大了 */
+    char kernelname[100];   /* 应该足够大了 */
     int isExist = 0;
     int i=0;
     int name_cnt=0;
     while( get_fs_byte(name+name_cnt) != '\0')
-			name_cnt++;
+            name_cnt++;
     if(name_cnt>SEM_NAME_LEN)
-			return NULL;
+            return NULL;
     for(i=0;i<name_cnt;i++)
-			kernelname[i]=get_fs_byte(name+i);
+            kernelname[i]=get_fs_byte(name+i);
     int name_len = strlen(name);
     int sem_name_len =0;
     for(i=0;i<cnt;i++)
@@ -98,11 +98,11 @@ int sys_sem_unlink(const char *name)
         sem_name_len = strlen(semtable[i].name);
         if(sem_name_len == name_len)
         {
-	    		if( !strcmp(kernelname,semtable[i].name))
-	    		{
-						isExist = 1;
-						break;
-	    		}
+                if( !strcmp(kernelname,semtable[i].name))
+                {
+                        isExist = 1;
+                        break;
+                }
         }
     }
     if(isExist == 1)
